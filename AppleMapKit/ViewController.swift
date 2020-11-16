6066d6f44ed50e9c53aea9dba460880d2331bfd1
SarooshNasir <72987691+SarooshNasir@users.noreply.github.com>
2020-11-16 15:40:52 +0500

    Initial Commit

---
+++ AppleMapKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppleMapKit
+//
+//  Created by IT on 11/16/20.
+//  Copyright © 2020 IT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
